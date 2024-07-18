b131dd79f90d9517a701d2cdc2862a8fc5f4edf9
Akhilthati <30937306+Akhilthati@users.noreply.github.com>
2024-07-18 13:00:40 -0400

    changes

--- components/project.tsx
+++ components/project.tsx
@@ -18,7 +18,6 @@ export default function Project({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
-
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
